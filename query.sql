e2bfa6c769f93d55bdd5218f03dfa8bc9f16eebc
minion-K <rnjsalsgus06@naver.com>
2025-08-11 16:24:43 +0900

    0811

--- query.sql
+++ query.sql
@@ -1,2 +1,16 @@
 # 1. 스키마 생성 (이미 존재하면 삭제)
 DROP DATABASE IF EXISTS k5_iot_springboot;
+
+# 2. 스키마 생성 + 문자셋/정렬 설정
+CREATE DATABASE IF NOT EXISTS k5_iot_springboot
+	CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+    
+# 3. 스키마 선택
+USE k5_iot_springboot;
+
+# 0811 (A_Test)
+CREATE TABLE IF NOT EXISTS test (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
