6820045bf233e3b99ceaaec47548b31181850257
minion-K <rnjsalsgus06@naver.com>
2025-08-19 10:49:46 +0900

    0819 Post / Comment Repository, Service, Controller

---
+++ src/main/java/com/example/k5_iot_springboot/service/impl/D_PostServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.k5_iot_springboot.service.impl;
+
+import com.example.k5_iot_springboot.service.D_PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class D_PostServiceImpl implements D_PostService {
+}
