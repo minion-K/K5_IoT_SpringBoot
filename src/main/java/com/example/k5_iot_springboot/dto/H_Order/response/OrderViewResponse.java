56784d2e0eb4775d3f839acc0eb741f7d60e665c
minion-K <rnjsalsgus06@naver.com>
2025-09-02 10:35:24 +0900

    0902 ORDER_PRODUCT_VIEW & DTO

---
+++ src/main/java/com/example/k5_iot_springboot/dto/H_Order/response/OrderViewResponse.java
@@ -0,0 +1,23 @@
+package com.example.k5_iot_springboot.dto.H_Order.response;
+
+public class OrderViewResponse {
+    public record OrderSummary(
+            Long orderId,
+            Long userId,
+            String orderStatus,
+            String productName,
+            Integer quantity,
+            Integer price,
+            Integer totalPrice,
+            String orderedAt
+    ) {}
+
+    public record OrderTotalsRowDto(
+            Long orderId,
+            Long userId,
+            String orderStatus,
+            Integer TotalAmount,
+            Long TotalQuantity,
+            String orderedAt
+    ) {}
+}
